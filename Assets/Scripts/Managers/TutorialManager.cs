7b21f465fd0b6473af26078d76a10f3cfcb44a39
Klarus <ciuk21@gmail.com>
2024-12-07 12:37:41 +0100

    Added tutorial

---
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialPanel;
+    [SerializeField] private Button tutorialEndButton;
+    private bool isTutorialEnded;
+
+    private void Start()
+    {
+        tutorialEndButton.onClick.AddListener(TutorialEnd);
+        Time.timeScale = 0;
+    }
+
+    private void TutorialEnd()
+    {
+        isTutorialEnded = true;
+        tutorialPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
