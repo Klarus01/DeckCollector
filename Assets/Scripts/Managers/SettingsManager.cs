66fee6bf3533868e1b13a7b119b10168d87fa172
Klarus <ciuk21@gmail.com>
2024-12-07 11:52:48 +0100

    Changes in settings

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
@@ -9,45 +10,22 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button closeButton;
-    [SerializeField] private Button exitGameButton;
+    [SerializeField] private Button mainMenuButton;
     [SerializeField] private Slider musicVolumeSlider;
     [SerializeField] private Slider effectsVolumeSlider;
     [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] private TMP_Dropdown fpsDropdown;
     [SerializeField] private Toggle fullscreenToggle;
     
-    private bool isGamePaused = false;
+    private bool isGamePaused;
     private Resolution[] availableResolutions;
 
     private void Start()
     {
-        if(settingsButton) settingsButton.onClick.AddListener(ToggleSettings);
-        closeButton.onClick.AddListener(CloseSettings);
-        exitGameButton.onClick.AddListener(ExitGame);
-        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
-        effectsVolumeSlider.onValueChanged.AddListener(SetEffectsVolume);
-        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
-        effectsVolumeSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 1f);
-        
-        availableResolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        var resolutionOptions = new List<string>();
-        foreach (var resolution in availableResolutions)
-        {
-            resolutionOptions.Add($"{resolution.width}x{resolution.height}");
-        }
-        resolutionDropdown.AddOptions(resolutionOptions);
-        resolutionDropdown.value = GetCurrentResolutionIndex();
-        resolutionDropdown.onValueChanged.AddListener(SetResolution);
-        
-        fullscreenToggle.isOn = Screen.fullScreen;
-        fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
-        
-        fpsDropdown.ClearOptions();
-        var fpsOptions = new List<string> { "Unlimited", "30", "60", "120" };
-        fpsDropdown.AddOptions(fpsOptions);
-        fpsDropdown.value = 1;
-        fpsDropdown.onValueChanged.AddListener(SetFPSLimit);
+        InitializeButtons();
+        InitializeSliders();
+        InitializeDropdowns();
+        InitializeToggle();
     }
 
     private void Update()
@@ -58,11 +36,72 @@ public class SettingsManager : MonoBehaviour
         }
     }
 
+    private void InitializeButtons()
+    {
+        if (settingsButton) settingsButton.onClick.AddListener(ToggleSettings);
+        closeButton.onClick.AddListener(CloseSettings);
+        if (SceneManager.GetActiveScene().name.Equals("MainMenu"))
+        {
+            mainMenuButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            mainMenuButton.gameObject.SetActive(true);
+            mainMenuButton.onClick.AddListener(BackToMainMenu);
+        }
+    }
+
+    private void InitializeSliders()
+    {
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        effectsVolumeSlider.onValueChanged.AddListener(SetEffectsVolume);
+
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        effectsVolumeSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 1f);
+    }
+
+    private void InitializeDropdowns()
+    {
+        InitializeResolutionDropdown();
+        InitializeFPSDropdown();
+    }
+
+    private void InitializeResolutionDropdown()
+    {
+        availableResolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        var resolutionOptions = new List<string>();
+        foreach (var resolution in availableResolutions)
+        {
+            resolutionOptions.Add($"{resolution.width}x{resolution.height}");
+        }
+
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = GetCurrentResolutionIndex();
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    private void InitializeFPSDropdown()
+    {
+        var fpsOptions = new List<string> { "Unlimited", "30", "60", "120" };
+        fpsDropdown.ClearOptions();
+        fpsDropdown.AddOptions(fpsOptions);
+        fpsDropdown.value = 1;
+        fpsDropdown.onValueChanged.AddListener(SetFPSLimit);
+    }
+
+    private void InitializeToggle()
+    {
+        fullscreenToggle.isOn = Screen.fullScreen;
+        fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
+    }
+
     private void ToggleSettings()
     {
         if (settingsPanel == null) return;
 
-        var isActive = settingsPanel.activeSelf;
+        bool isActive = settingsPanel.activeSelf;
         settingsPanel.SetActive(!isActive);
 
         isGamePaused = !isActive;
@@ -79,7 +118,7 @@ public class SettingsManager : MonoBehaviour
         Time.timeScale = 1;
         isGamePaused = false;
     }
-    
+
     private void SetMusicVolume(float value)
     {
         PlayerPrefs.SetFloat("MusicVolume", value);
@@ -89,7 +128,7 @@ public class SettingsManager : MonoBehaviour
     {
         PlayerPrefs.SetFloat("EffectsVolume", value);
     }
-    
+
     private int GetCurrentResolutionIndex()
     {
         for (int i = 0; i < availableResolutions.Length; i++)
@@ -99,10 +138,10 @@ public class SettingsManager : MonoBehaviour
         }
         return 0;
     }
-    
+
     private void SetResolution(int index)
     {
-        var resolution = availableResolutions[index];
+        Resolution resolution = availableResolutions[index];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
@@ -113,24 +152,11 @@ public class SettingsManager : MonoBehaviour
 
     private void SetFPSLimit(int index)
     {
-        switch (index)
-        {
-            case 0:
-                Application.targetFrameRate = -1;
-                break;
-            case 1:
-                Application.targetFrameRate = 30;
-                break;
-            case 2:
-                Application.targetFrameRate = 60;
-                break;
-            case 3:
-                Application.targetFrameRate = 120;
-                break;
-        }
+        int[] fpsValues = { -1, 30, 60, 120 };
+        Application.targetFrameRate = fpsValues[index];
     }
 
-    private void ExitGame()
+    private void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
